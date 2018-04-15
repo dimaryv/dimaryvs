dc090af6e80404e4e0c89451f215bc4eb978ade2
dimaryv <37155068+dimaryv@users.noreply.github.com>
2018-04-15 18:50:20 +0300

    Update EncryDecryImg.java

--- EncryDecryImg.java
+++ EncryDecryImg.java
@@ -4,18 +4,23 @@ import java.nio.file.Paths;
 import java.util.Scanner;
 
 
-public class Main {
+public class EncryDecryImg {
         public static void main(String args[]){
 
             Scanner scan = new Scanner(System.in);
             System.out.print("Variant: ");
 
-            StringBuilder Var = new StringBuilder(scan.nextLine());
-            if(Var.length() == 1)
-            {
+            StringBuffer Var = new StringBuffer(scan.nextLine());
+            if(Var.length() == 1) {
+                if(Var.toString().matches("\\D")) {
+                    System.out.print("No matches.Try Again.");
+                    return;
+                }
                 Var.insert(0, 0);
             }
-            else if(Var.length() > 2 || Var.length() < 0){
+            else if(Var.length() > 2 || Var.length() < 0 ||
+                    Var.toString().matches("\\D(.*)") ||
+                    Var.toString().matches("(.*)\\D") || Var.toString().matches("[3-9](.*)")){
                 System.out.print("No matches.Try Again.");
                 return;
             }
@@ -26,9 +31,18 @@ public class Main {
                 byte[] ByteFileArray = Files.readAllBytes(Paths.get("src/pictures/img"+ Var +".jpg.encrypted"));
                 byte[] ByteKeyArray = "qwerty".getBytes();
 
+                double a = Math.round(0.03 * ByteFileArray.length);
+                System.out.println("Proccesing...");
+                int counter = 0;
+                System.out.print("|");
                 for(int i = 0; i < ByteFileArray.length; i++) {
                     fos.write(ByteFileArray[i] ^ ByteKeyArray[i % ByteKeyArray.length]);
+                    if(counter == i){
+                        System.out.print("=");
+                        counter += a;
+                    }
                 }
+                System.out.print("| \n Done!");
             }
             catch(IOException e){
                 System.err.println(e.toString());
