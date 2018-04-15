3396178a4999ee39fa961ad03ec9e05eaef9638a
dimaryv <37155068+dimaryv@users.noreply.github.com>
2018-04-15 15:44:46 +0300

    Update EncryDecryImg.java

--- EncryDecryImg.java
+++ EncryDecryImg.java
@@ -1,15 +1,29 @@
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 
-public class EncryDecryImg {
+public class Main {
         public static void main(String args[]){
+
+            Scanner scan = new Scanner(System.in);
+            System.out.print("Variant: ");
+
+            StringBuilder Var = new StringBuilder(scan.nextLine());
+            if(Var.length() == 1)
+            {
+                Var.insert(0, 0);
+            }
+            else if(Var.length() > 2 || Var.length() < 0){
+                System.out.print("No matches.Try Again.");
+                return;
+            }
             try{
-                File outfile = new File("src/pictures/encrypted_img18.jpg");
+                File outfile = new File("src/pictures/encrypted_img"+ Var +".jpg");
                 FileOutputStream fos = new FileOutputStream(outfile);
 
-                byte[] ByteFileArray = Files.readAllBytes(Paths.get("src/pictures/img18.jpg.encrypted"));
+                byte[] ByteFileArray = Files.readAllBytes(Paths.get("src/pictures/img"+ Var +".jpg.encrypted"));
                 byte[] ByteKeyArray = "qwerty".getBytes();
 
                 for(int i = 0; i < ByteFileArray.length; i++) {
