659c5c7319e277ab44d7ecb7ff246450f5fbdbd9
dimaryv <37155068+dimaryv@users.noreply.github.com>
2019-04-11 10:48:37 +0300

    Update database.php

--- database.php
+++ database.php
@@ -1,4 +1,48 @@
 <?php
+/*
+echo('Модель запущена <br>');
+
+$mysqli = new mysqli('localhost','root','','tablesitedb');
+if(mysqli_connect_errno()) {
+  echo ('Error occured('.mysqli_connect_errno().'): '.mysqli_connect_error());
+  exit();
+}
+echo 'Successfully connected to server! <br>';
+$json_array = array();
+if ($result = $mysqli->query('SELECT * FROM categories')) {
+while ($row = $result->fetch_assoc()) {
+  $json_array[] = $row;
+}
+  $result -> free();
+}
+$mysqli -> close();
+$data = json_encode($json_array);
+
+*/
+function get_data() {
+
+  $mysqli = new mysqli('localhost','root','','tablesitedb');
+  if(mysqli_connect_errno()) {
+    echo ('Error occured('.mysqli_connect_errno().'): '.mysqli_connect_error());
+    exit();
+  }
+  $json_array = array();
+  if ($result = $mysqli->query('SELECT * FROM categories')) {
+    while ($row = $result->fetch_assoc()) {
+        $json_array[] = $row;
+        }
+      $result -> free();
+  }
+  $mysqli -> close();
+
+  $jar = json_encode($json_array);
+
+  return $jar;
+
+}
+
+//Старая модель
+/*  echo("Модель запущена");
   $link = mysqli_connect('localhost','root','','tablesitedb');
 
   if(mysqli_connect_errno()) {
@@ -7,14 +51,18 @@
   }
   else{
       printf("Succes!\n");
-      $query = 'SELECT * FROM tablesitedb';
       if($result = $link -> query("SELECT val FROM categories")) {
           printf(" vernul %d strok ", $result -> num_rows);
       }
   }
-
+  $sql = mysqli_query($link, 'INSERT INTO categories (name, surname, place) VALUES ("Mihail_Petrovich","Zubenko","Shumilovka_Gorodok")');
   $sql = mysqli_query($link, 'SELECT val, name, surname, place FROM categories');
   while ($result = mysqli_fetch_array($sql)) {
-    echo ("{$result['val']}: {$result['name']}, {$result['surname']},{$result['place']}");
+    echo ("<br>{$result['val']}: {$result['name']}, {$result['surname']},{$result['place']}<br>");
   }
+  $sql = mysqli_query($link, 'SELECT * FROM categories');
+  $result = mysqli_fetch_array($sql);
+  $data = json_encode($result);
+  echo ($data);
+*/
 ?>
