19ece451d95a1aa501bfa251096176958720840a
dimaryv <37155068+dimaryv@users.noreply.github.com>
2019-04-11 10:49:07 +0300

    Create controller.php

---
+++ controller.php
@@ -0,0 +1,7 @@
+<?php
+include_once('C:\xampp\htdocs\TableSite\drupal-7.65\sites\all\modules\TestTableView\model\database.php');
+//include_once('C:\xampp\htdocs\TableSite\drupal-7.65\sites\all\modules\TestTableView\view\view.php');
+    function list_data() {
+        return get_data();
+    }
+?>
